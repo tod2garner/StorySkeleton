8c7c1f8f70cb0e5900a561ed7f50fa6c0b1f04fc
Ackherst <tod2garner@gmail.com>
2019-11-03 15:03:12 -0500

    Partial check-in, directional trust prereq.

--- StoryEngine/Prerequisites/DirectionalTrust.cs
+++ StoryEngine/Prerequisites/DirectionalTrust.cs
@@ -6,22 +6,22 @@ using System.Threading.Tasks;
 
 namespace StoryEngine
 {
-    public class DirectionalTrust : ACrossRolePrerequisite
+    public abstract class DirectionalTrust : ACrossRolePrerequisite
     {
-        protected EthicsScale benchmarkTrust_Forward;//from A to B
-        protected EthicsScale benchmarkTrust_Returned;//from B to A
+        protected EthicsScale benchmarkTrust_AtoB;
 
-        public DirectionalTrust(IncidentRole roleA, IncidentRole roleB, EthicsScale trust_AtoB, EthicsScale trust_BtoA)
+        public DirectionalTrust(IncidentRole roleA, IncidentRole roleB, EthicsScale trust_AtoB)
         {
-            benchmarkTrust = benchmark;
-            this.role = whichRole;
+            benchmarkTrust_AtoB = trust_AtoB;
+            this.roleAlpha = roleA;
+            this.roleBeta = roleB;
         }
 
         public override bool IsMetByCurrentParticipants()
         {
-            foreach (Character a in role.Participants)
+            foreach (Character a in roleAlpha.Participants)
             {
-                if (role.Participants.Any(b => (a.Id != b.Id) && (HaveMutualTrustThatPassesBenchmark(a, b) == false)))
+                if (roleBeta.Participants.Any(b => PassesBenchmark(a.GetTrustTowards(b.Id)) == false))
                     return false;
             }
 
@@ -31,6 +31,38 @@ namespace StoryEngine
         public override bool TryToFulfillFromScratch(SocietySnapshot currentCast, Random rng = null)
         {
             throw new NotImplementedException();
+        }
+
+        protected virtual bool PassesBenchmark(EthicsScale? theValue)
+        {
+            if (theValue.HasValue == false) //#TODO - overwrite method for situations where a null relationship would meet benchmark
+                return false;
+
+            return PassesBenchmark(theValue.Value);
+        }
+
+        protected abstract bool PassesBenchmark(EthicsScale theValue);
+    }
+
+    public class DirectionalTrust_Min : DirectionalTrust
+    {
+        /// <param name="minimum">Inclusive minimum trust value</param>
+        public DirectionalTrust_Min(IncidentRole roleA, IncidentRole roleB, EthicsScale minimum) : base(roleA, roleB, minimum) { }
+
+        protected override bool PassesBenchmark(EthicsScale value)
+        {
+            return value >= this.benchmarkTrust_AtoB;
+        }
+    }
+
+    public class DirectionalTrust_Max : MutualTrust
+    {
+        /// <param name="maximum">Inclusive maximum trust value</param>
+        public DirectionalTrust_Max(IncidentRole roleA, IncidentRole roleB, EthicsScale maximum) : base(roleA, roleB, maximum) { }
+
+        protected override bool PassesBenchmark(EthicsScale value)
+        {
+            return value <= this.benchmarkTrust_AtoB;
         }
     }
 }
