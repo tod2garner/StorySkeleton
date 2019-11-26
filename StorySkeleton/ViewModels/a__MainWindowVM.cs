63c679bd19bfe2c3be9db163cc09bd835c093818
tod2g <tod2garner@gmail.com>
2019-11-26 07:00:56 -0500

    Continued rough 1st pass at UI

---
+++ StorySkeleton/ViewModels/a__MainWindowVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorySkeleton.ViewModels
+{
+    public class MainWindowVM
+    {
+        public PlotGeneratorVM TheGenerator;
+        public PlotVM ThePlot;
+    }
+}
--- StorySkeleton/Views/Characters/NewCharacterView.xaml.cs
+++ StorySkeleton/Views/Characters/NewCharacterView.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace StorySkeleton.Views
+namespace StorySkeleton.Views.Characters
 {
     /// <summary>
     /// Interaction logic for NewCharacter.xaml
